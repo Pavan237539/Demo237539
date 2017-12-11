f558034b5aa7dc683d9c61c965b22c245242b070
pavan <sun237539@gmail.com>
2017-12-11 18:24:34 +0530

    project commit sucessfully

---
+++ onlineshopping/src/main/java/org/pav/onlineshopping/Controller/PageController.java
@@ -0,0 +1,18 @@
+package org.pav.onlineshopping.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value={"/","/home","/index"})
+	public ModelAndView index(){
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("greeting", "Hello SpringMVC!!");
+		return mv;
+	}
+	
+	
+}
